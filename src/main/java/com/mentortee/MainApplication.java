61522c4dc5f0690d2b77bebd2811a7ba661d90bb
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-04-10 11:07:54 +0900

    feat: initial project setting

---
+++ src/main/java/com/mentortee/MainApplication.java
@@ -0,0 +1,14 @@
+package com.mentortee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
+
