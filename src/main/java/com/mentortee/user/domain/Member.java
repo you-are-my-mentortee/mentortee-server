57a38c82bce32c1eaa6310af44d615205c668a76
jola7373 <62223855+jola7373@users.noreply.github.com>
2022-04-10 19:45:32 +0900

    feat:add anotation Member domain

--- src/main/java/com/mentortee/user/domain/Member.java
+++ src/main/java/com/mentortee/user/domain/Member.java
@@ -1,12 +1,13 @@
 package com.mentortee.user.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
 public class Member {
 
-
+    @Id
     private UUID userId;
 
     private String nickName;
